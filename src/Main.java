c0853199ef598b28eb0f4a5d713786740e9bb63a
Enotich Enotov <enotichenotov195@gmail.com>
2022-10-04 17:29:51 +0300

    task 2; first commit was wrong, remade commit

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,26 @@
-//Решение не полное, постараюсь до вторника исправить
 import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in).useLocale(Locale.ENGLISH);
         int n = in.nextInt();
-        int x = in.nextInt();
-        String str = Integer.toString(n);
-        int[] arr = new int[str.length()-1];
-        for (int i = 0; i <= str.length(); ++i){
-            if (x != n % 10){
-                arr[i] = n %10;
-            }
-            else{
-                System.out.print(n/10);
-                break;
-            }
-            n /= 10;
+        int sig = 1;
+        if (n < 0){
+            sig = -1;
+            n *= -1;
         }
-        for (int i = arr.length-1; i >= 0; --i){
-            if (arr[i]==0) {
-                continue;
-            }
-            System.out.print(arr[i]);
+        int a = n;
+        int k = 0;
+        while (a > 0){
+            k += 1;
+            a /= 10;
         }
-
+        a = 0;
+        for (int i = k-1; i >=0; --i){
+            a += (n % 10) * Math.pow(10, i);
+            n = n / 10;
+        }
+        a*=  sig;
+        System.out.println(a);
     }
-
 }
-
